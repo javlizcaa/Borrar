8311e292b0bbdff44b97ece8368951b8386d329d
Javi <100451699@alumnos.uc3m.es>
2023-04-30 17:36:06 +0200

    update information

--- app/src/main/java/com/example/borrar/EditProfile.java
+++ app/src/main/java/com/example/borrar/EditProfile.java
@@ -2,11 +2,16 @@ package com.example.borrar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,12 +22,13 @@ import com.example.borrar.db.dbHelper_User;
 public class EditProfile extends AppCompatActivity {
 
     TextView edit_name,edit_surname,edit_email,edit_password,edit_age,edit_height,edit_weight;
+    Button edit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
-        //Edir profile
+        //set user information
 
         edit_name=findViewById(R.id.edit_name);
         edit_surname=findViewById(R.id.edit_surname);
@@ -40,6 +46,37 @@ public class EditProfile extends AppCompatActivity {
         edit_height.setText(Float.toString(user.getHeight()));
         edit_weight.setText(Float.toString(user.getWeight()));
         edit_age.setText(Integer.toString(user.getAge()));
+
+        //edit profile
+
+        edit=findViewById(R.id.editBt);
+
+        edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dbHelper_User dbHelper=new dbHelper_User(EditProfile.this);
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+                ContentValues values = new ContentValues();
+                values.put(BBDD_User.COLUMN_name, edit_name.getText().toString());
+                values.put(BBDD_User.COLUMN_surname, edit_surname.getText().toString());
+                values.put(BBDD_User.COLUMN_mail, edit_email.getText().toString());
+                values.put(BBDD_User.COLUMN_password, edit_password.getText().toString());
+                values.put(BBDD_User.COLUMN_age, edit_age.getText().toString());
+                values.put(BBDD_User.COLUMN_height, edit_height.getText().toString());
+                values.put(BBDD_User.COLUMN_weight, edit_weight.getText().toString());
+                int numRows = db.update(BBDD_User.TABLE_NAME, values, "id = ?", new String[] { String.valueOf(user.getId()) });
+
+                if (numRows > 0) {
+                    Toast.makeText(getApplicationContext(),"Information correctly updated", Toast.LENGTH_LONG).show();
+                } else {
+                    Log.d("SQLite", "information could not be updated");
+                }
+                db.close();
+            }
+        });
+
 
 
     }
